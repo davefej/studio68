3009852e816acee2c97a3d402cde2cb410e271aa
davefej <davefej@gmail.com>
2017-03-03 23:24:35 +0100

    bug fix

--- common/header.php
+++ common/header.php
@@ -3,15 +3,15 @@
 	
 		<link href="https://fonts.googleapis.com/css?family=Muli:300|Raleway:300,900|Source+Code+Pro|Lora:400i" rel="stylesheet">
 		<link href="../studio68/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		 
-		<link rel="stylesheet" href="css/contact-style.css">
-		<link rel="stylesheet" href="css/vegas.min.css">
-		<link rel="stylesheet" href="css/footer.css">
-		<link rel="stylesheet" href="css/nav-style.css">
+		 <?php $version = rand();?>
+		<link rel="stylesheet" href="css/contact-style.css?v=<?php echo $version;?>">
+		<link rel="stylesheet" href="css/vegas.min.css?v=<?php echo $version;?>">
+		<link rel="stylesheet" href="css/footer.css?v=<?php echo $version;?>">
+		<link rel="stylesheet" href="css/nav-style.css?v=<?php echo $version;?>">
 		
-		<link rel="stylesheet" href="css/team-style.css">
-		<link rel="stylesheet" href="css/departments-style.css">
-		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/team-style.css?v=<?php echo $version;?>">
+		<link rel="stylesheet" href="css/departments-style.css?v=<?php echo $version;?>">
+		<link rel="stylesheet" href="css/styles.css?v=<?php echo $version;?>">
 		
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="UTF-8">
