87eece824c27695b4bfd908261f60b9299e4a959
David Szanto <szavid0@gmail.com>
2017-05-30 16:31:17 +0200

    Start of the changes (unfinished)

--- content/common/navbar.php
+++ content/common/navbar.php
@@ -10,11 +10,19 @@
 			</div>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="index.php" class="nav_element">STÚDIÓ <?php if($page=="gift"){?><font color="black"> <?php }else{ ?> <font color="#e00901"><?php } ?>68</font></a></li>
-					<li><a href="eco.php" class="nav_element">ÖKO</a></li>
-					<li><a href="sweet.php" class="nav_element">ÉDESSÉG</a></li>
-					<li><a href="gift.php" class="nav_element">AJÁNDÉK</a></li>
-					<li><a href="fan.php" class="nav_element">SPORT</a></li>
+					<li>
+                        <a href="index.php" class="nav_element">STÚDIÓ <?php if($page=="gift"){?><font color="black"> <?php }else{ ?> <font color="#e00901"><?php } ?>68</font></a>
+                    </li>
+					<li class="dropdown">
+                      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">ÜZLETÁGAK<span class="caret"></span></a>
+                          <ul class="dropdown-menu">
+                            <li><a href="eco.php" class="nav_element">ÖKO</a></li>
+                            <li><a href="sweet.php" class="nav_element">ÉDESSÉG</a></li>
+                            <li><a href="gift.php" class="nav_element">AJÁNDÉK</a></li>
+                            <li><a href="fan.php" class="nav_element">SPORT</a></li>
+                          </ul>
+                    </li>
+                    
 					<li><a href="printing.php" class="nav_element">NYOMDA</a></li>
 					<li><a href="eshop.php" class="nav_element">E-BOLT</a></li>
 					<li><a href="blog.php" class="nav_element">BLOG</a></li>
--- content/desktop/eshop.php
+++ content/desktop/eshop.php
@@ -1,2 +1,61 @@
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-<h1>TODO</h1>
+<div class="main_segment container-fluid" id="main">
+
+    <div class="eshop-title">
+        Promóciónk örömére...
+
+        <hr style="width: 10%; border-color:#333;">
+
+    </div>
+
+    <div class="eshop-intro">
+        Környezettudatos termékeink nem pusztán divatból kerültek be kínálatunkba.
+        Valljuk, hogy természetünk drága kincs, melyet óvni kell, nemcsak beszélni róla.
+        Amennyiben Ön is elkötelezett a természetvédelem területén, itt a lehetőség, hogy együtt forradalmasíthassuk meg az öko tudatos gondolkodást.
+        Rendeljen promóciós céljaihoz zöld termékeket.
+        Itt minden újrahasznosított. Vagy vízzel működik. Illetve napelemmel. Esetleg dinamóval.
+        Görget. És…
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div onclick="" class="well" id="energy">
+                    <div class="energy-open">
+                        <b >Zöld Energia</b>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div onclick="" class="well" id="textil">
+                    <div class="textil-open">
+                        <b>Újrahasznosított Termékek</b>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div onclick="" class="well" id="ideas">
+                    <div class="ideas-open">
+                        <b>Környezetbarát Ötletek</b>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="well describe" id="energy-describe">
+                  #dinamóslámpa #napelemesszámológép #vízenergiásóra #napelemespowerbank #bambuszpendrive #kartonfalióra #ökotöltő #nemkellelem #napenergianeked #solar4you
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="well describe" id="textil-describe">
+                  #farmertoll #gumiabroncsegérpad #papírmappa #ökotudatos #ceruzabankjegyből #bambuszkészlet #gondoldújra #nebántsdafát #kukoricaalapúműanyag #mindenamiöko
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="well describe" id="textil-describe">
+                 #vászontáska #bionövény #organikuspóló #fafejtörőjáték #madárodúmásképp #környzetbarátkulacs #ökobevásárlótáska #használdújra #ökostúdió68 #atermészetmentők
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
